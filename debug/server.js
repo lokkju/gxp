d704a0517d874f657f019ccb3dd7f92fe53e8263
lokkju <lokkju@gmail.com>
2013-11-18 12:06:36 -0800

    adding nodejs debug/test server

---
+++ debug/server.js
@@ -0,0 +1,32 @@
+/**
+ * server.js
+ *
+ * Simple Node server for developing HTML clients
+ */
+
+var connect = require('connect'),
+    path = require('path')
+    httpProxy = require('http-proxy'),
+    routingProxy = new httpProxy.RoutingProxy(),
+    app = connect()
+
+var geoserverProxy = function (pattern, host, port) {
+    return function (req, res, next) {
+        if (req.url.match(pattern)) {
+            routingProxy.proxyRequest(req, res, {host: host, port: port})
+        } else {
+            next();
+        }
+    }
+}
+
+app.use(connect.logger('dev'))
+app.use(geoserverProxy(/\/geoserver\/.*/, process.env.GEOSERVER_HOST || 'arcgisrs', process.env.GEOSERVER_PORT || 8080))
+app.use(connect.favicon())
+app.use(connect.static(path.resolve(__dirname ,'../')))
+app.use(connect.directory(path.resolve(__dirname ,'../')))
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Connect Server listening on port ' + (process.env.PORT || 3000))
+  console.log('Serving content from ' + path.resolve(__dirname ,'../'))
+});
